4138e9f5dc694d0f00c0212187e345d48bbc9bfe
Kashan Haider <hkashan.dev@gmail.com>
2025-07-25 21:44:55 +0500

    fixed env variables issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,15 @@ import sanity from '@sanity/astro';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
+import dotenv from 'dotenv';
+
+dotenv.config(); // default loads `.env`
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [sanity({
-    projectId: '5kin5owf',
-    dataset: 'production',
+    projectId: process.env.PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.PUBLIC_SANITY_DATASET,
     useCdn: false, // See note on using the CDN
     apiVersion: "2025-01-28", // insert the current date to access the latest version of the API
   studioBasePath: '/studio'
